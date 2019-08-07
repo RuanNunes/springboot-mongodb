65be849acc0a7afec7b1058f7c47aa8d369933b1
Ruan Nunes <ruanschwanz@gmail.com>
2019-08-07 20:44:10 -0300

    Consulta simples com query methods

--- src/main/java/com/ruan/nunes/mongodb/resources/PostResource.java
+++ src/main/java/com/ruan/nunes/mongodb/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ruan.nunes.mongodb.domain.Post;
+import com.ruan.nunes.mongodb.resources.util.URL;
 import com.ruan.nunes.mongodb.services.PostService;
 
 @RestController
@@ -30,5 +32,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post post = postService.finById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue =  "") String text){
+		text = URL.decodeParam(text);
+		return ResponseEntity.ok().body(postService.findByTitle(text));
 	}	
 }
