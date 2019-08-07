07cbf099d90fd784a720587e862b5ec28c30898f
Ruan Nunes <ruanschwanz@gmail.com>
2019-08-07 16:42:07 -0300

    Referenciando posts ao usuario

--- src/main/java/com/ruan/nunes/mongodb/config/Instantiation.java
+++ src/main/java/com/ruan/nunes/mongodb/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viage", "Vou viajar para londres abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/03/2019"), "Cheguei da viage", "cheguei muito bem em londres amigos", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
