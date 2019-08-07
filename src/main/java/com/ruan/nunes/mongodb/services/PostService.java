44ffd0a5d177c5281574455dff09cf9fc6248157
Ruan Nunes <ruanschwanz@gmail.com>
2019-08-07 20:02:54 -0300

    obtendo post por id

---
+++ src/main/java/com/ruan/nunes/mongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.ruan.nunes.mongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruan.nunes.mongodb.domain.Post;
+import com.ruan.nunes.mongodb.repository.PostRepository;
+import com.ruan.nunes.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired 
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post finById(String id) {
+		Optional<Post> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
