932f9df46e8d70e5a769152aff85ae92bb018b13
Ruan Nunes <ruanschwanz@gmail.com>
2019-08-07 20:18:26 -0300

    Adicionando comentarios aos posts

--- src/main/java/com/ruan/nunes/mongodb/config/Instantiation.java
+++ src/main/java/com/ruan/nunes/mongodb/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.ruan.nunes.mongodb.DTO.AuthorDTO;
+import com.ruan.nunes.mongodb.DTO.CommentDTO;
 import com.ruan.nunes.mongodb.domain.Post;
 import com.ruan.nunes.mongodb.domain.User;
 import com.ruan.nunes.mongodb.repository.PostRepository;
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("BR"));
 		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viage", "Vou viajar para londres abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/03/2019"), "Cheguei da viage", "cheguei muito bem em londres amigos", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem Maria", sdf.parse("21/03/2019"), new AuthorDTO(bob));
+		CommentDTO c2 = new CommentDTO("Vai com Deus maria Maria", sdf.parse("21/03/2019"), new AuthorDTO(alex));
+		CommentDTO c3 = new CommentDTO("Fica Bem ai maria", sdf.parse("21/03/2019"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
